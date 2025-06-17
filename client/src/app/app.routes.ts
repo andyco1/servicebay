58710ed4b561da1db1ff7fcd2642a53b2e08564b
andyco1 <arc29@aber.ac.uk>
2025-06-17 20:13:27 +0100

    feat: inital mockup of login page and routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            // dashboard, calendar, etc. will go here later
+        ]
+    }
+];
