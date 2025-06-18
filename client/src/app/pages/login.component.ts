0ea4650c3d04c369a084f4431365e83f5339b0ec
andyco1 <arc29@aber.ac.uk>
2025-06-19 00:13:48 +0100

    feat: login page form

--- client/src/app/pages/login.component.ts
+++ client/src/app/pages/login.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AuthState } from '../state/auth/auth.models';
+import * as AuthActions from '../state/auth/auth.actions';
+import { selectAuthError, selectAuthLoading } from '../state/auth/auth.selectors';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
+    public form: FormGroup;
+    public loading$!: Observable<boolean>;
+    public error$!: Observable<string | null>;
 
+    constructor(
+        private fb: FormBuilder,
+        private store: Store<{ auth: AuthState }>
+    ) {
+        this.form = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
+
+        this.loading$ = this.store.select(selectAuthLoading);
+        this.error$ = this.store.select(selectAuthError);
+    }
+
+    public onSubmit(): void {
+        if (this.form.valid) {
+            const { email, password } = this.form.value;
+            this.store.dispatch(AuthActions.login({ email, password }));
+        } else {
+            this.form.markAllAsTouched();
+        }
+    }
 }
